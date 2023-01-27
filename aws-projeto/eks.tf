6798aaa5e50272e34d1e2c1f50fa797685b94bfb
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-27 19:03:09 -0300

    jkfile

--- aws-projeto/eks.tf
+++ aws-projeto/eks.tf
@@ -1,29 +1,6 @@
-resource "aws_iam_role" "demo" {
-  name = "eks-cluster-demo"
-
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-POLICY
-}
-
-resource "aws_iam_role_policy_attachment" "demo-AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.demo.name
-}
-
 resource "aws_eks_cluster" "demo" {
-  name     = "demo"
+  name     = "devopselite"
+ cluster_version = "1.23"
   role_arn = aws_iam_role.demo.arn
 
   vpc_config {
--- aws-projeto/node.tf
+++ aws-projeto/node.tf
@@ -1,33 +1,3 @@
-resource "aws_iam_role" "nodes" {
-  name = "eks-node-group-nodes"
-
-  assume_role_policy = jsonencode({
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-    Version = "2012-10-17"
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "nodes-AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.nodes.name
-}
-
-resource "aws_iam_role_policy_attachment" "nodes-AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.nodes.name
-}
-
-resource "aws_iam_role_policy_attachment" "nodes-AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.nodes.name
-}
-
 resource "aws_eks_node_group" "public-nodes" {
   cluster_name    = aws_eks_cluster.demo.name
   node_group_name = "public-nodes"
@@ -38,8 +8,10 @@ resource "aws_eks_node_group" "public-nodes" {
     aws_subnet.subnet_public_1a.id
   ]
 
+  ami_type  = "AL2_x86_64"
   capacity_type  = "ON_DEMAND"
-  instance_types = ["t3.medium"]
+  instance_types = ["t3.large"]
+  disk_size      = "50"
 
   scaling_config {
     desired_size = 1
@@ -55,35 +27,9 @@ resource "aws_eks_node_group" "public-nodes" {
     role = "general"
   }
 
-    taint {
-    key    = "team"
-     value  = "devops"
-     effect = "NO_SCHEDULE"
-   }
-
-  launch_template {
-    name    = aws_launch_template.eks-with-disks.name
-   version = aws_launch_template.eks-with-disks.latest_version
- }
-
   depends_on = [
     aws_iam_role_policy_attachment.nodes-AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.nodes-AmazonEKS_CNI_Policy,
     aws_iam_role_policy_attachment.nodes-AmazonEC2ContainerRegistryReadOnly,
   ]
 }
-
- resource "aws_launch_template" "eks-with-disks" {
-   name = "eks-with-disks"
-
-   key_name = "minhaaws"
-
-   block_device_mappings {
-    device_name = "/dev/xvdb"
-
-    ebs {
-     volume_size = 50
-    volume_type = "gp2"
-    }
-  }
- }
